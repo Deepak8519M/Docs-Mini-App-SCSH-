7976fcc6002d7b994397ec52b71fd74820ab701e
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-18 15:53:21 +0530

    Card Comp added

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Card() {
+  return <div className="w-60 h-72 bg-sky-200 rounded-lg"></div>;
+}
+
+export default Card;
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Card from "./Card";
 
 function Foreground() {
-  return <div className="w-full h-screen fixed top-0 left-0 z-[3]  "></div>;
+  return (
+    <div className="w-full h-screen fixed top-0 left-0 z-[3]  ">
+      <Card />
+    </div>
+  );
 }
 
 export default Foreground;
