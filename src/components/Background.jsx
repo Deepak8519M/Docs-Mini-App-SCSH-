aa4ad1f7c4caf8675fce19b983911e0558432ad1
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-18 12:54:01 +0530

    Background.jsx commp created code added

---
+++ src/components/Background.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Background() {
+  return (
+    <>
+      <div className="w-full py-7 flex justify-center text-zinc-500 text-xl  font-semibold absolute top-[5%] ">
+        Documents.
+      </div>
+      <h1 className="text-zinc-900  text-[13vw] leading-none tracking-normal font-semibold absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
+        Docs.
+      </h1>
+    </>
+  );
+}
+
+export default Background;
