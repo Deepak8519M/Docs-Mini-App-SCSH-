309b3e33ed179ca09ab746619ca74be8d3e96589
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-18 12:57:17 +0530

    code changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Background from "./components/Background";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-zinc-800 relative">
-      
+    <div className="w-full h-screen bg-zinc-800 relative cursor-pointer">
+      <Background />
+      <div className="w-full h-screen bg-sky-800"></div>
     </div>
   );
 }
